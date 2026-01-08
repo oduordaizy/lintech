871c864e7e0955765d38257fe9b1af8d22c68b51
achdaisy <oduordaizy@gmail.com>
2026-01-08 17:46:50 +0300

    more info for llms

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,17 @@ export default function robots(): MetadataRoute.Robots {
     const baseUrl = 'https://lintechwebsolutions.co.ke' // Replace with your actual domain
 
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/', // Example of a disallowed path
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/private/',
+            },
+            {
+                userAgent: ['GPTBot', 'Google-Extended', 'CCBot'],
+                allow: '/',
+            }
+        ],
         sitemap: `${baseUrl}/sitemap.xml`,
     }
 }
